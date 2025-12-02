c4e8b9f5c15cf05bc97ccb2b1fd58c854d737e19
Shamsuzzoha <33594007+ArbiKhan007@users.noreply.github.com>
2025-12-02 18:49:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // This is the boilerplate code given for you
 // You can modify this code
 // Product data
-document.addEventListener('DOMContentLoaded', function() {
-	const products = [
+const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
   { id: 3, name: "Product 3", price: 30 },
@@ -73,5 +72,4 @@ function clearCart() {
 
 // Initial render
 renderProducts();
-renderCartFromSession();      
-});
+renderCartFromSession();
